bc3a2eb2e97439da67f9f576dd6ad09db2d11181
Rirog <156835164+Rirog@users.noreply.github.com>
2025-01-20 11:06:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """13 DZ"""
-from typing import Union
+from typing import Union, Dict
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -7,7 +7,7 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
-# Модели данных
+
 class Person(BaseModel):
     """Создание базовой модели данных"""
     name: str
@@ -24,78 +24,48 @@ class GraduateStudent(Person):
     thesis_topic: str
 
 
-# Хранилище данных
 students = {}
 
 
-# Эндпоинт для добавления нового студента
 @app.post("/students/")
-def create_student(student: Union[Student, GraduateStudent]):
+async def create_student(student: Union[Student, GraduateStudent]):
     """Создает нового студента"""
-    student_id_counter = 1
-    student_id = student_id_counter
-    student_id_counter += 1
-
-    if isinstance(student, GraduateStudent):
-        students[student_id] = {
-            "name": student.name,
-            "age": student.age,
-            "grades": [],
-            "thesis_topic": student.thesis_topic,
-        }
-    else:
-        students[student_id] = {
-            "name": student.name,
-            "age": student.age,
-            "grades": student.grades,
-            "thesis_topic": None,
-        }
+    student_id = len(students) + 1
+    students[student_id] = student
 
     return {"id": student_id, "message": "Student added successfully"}
 
 
-# Эндпоинт для добавления оценки студенту
+
 @app.put("/students/{student_id}/grades/")
-def add_grade(student_id: int, grade: int):
+async def add_grade(student_id: int, grade: int):
     """Добавляет оценку студенту"""
     if student_id not in students:
         return {"error": "Student not found"}
-    students[student_id]["grades"].append(grade)
+    students[student_id].grades.append(grade)
     return {"message": "Grade added successfully"}
 
 
-# Эндпоинт для получения списка студентов, отсортированных по средней оценке
+
 @app.get("/students/")
-def get_students_sorted_by_average_grade():
+async def get_students_sorted_by_average_grade():
     """Возращает список студентов, отсортированных по средней оценке"""
+    student: Union[Student, GraduateStudent]
     sorted_students = sorted(
         students.values(),
-        key=lambda x:
-        sum(x["grades"]) / len(x["grades"]) if x["grades"] else 0,
+       key=lambda student: (
+            sum(student.grades) / len(student.grades) if isinstance(student, Student) and student.grades else 0
+        ),
         reverse=True,
     )
 
-    return [
-        {
-            "name": student["name"],
-            "age": student["age"],
-            "average_grade":
-            sum(student["grades"]) / len(student["grades"])
-            if student["grades"] else 0,
-            "thesis_topic": student["thesis_topic"],
-        }
-        for student in sorted_students
-    ]
+    return [ student.dict()
+        for student in sorted_students]
+
 
 
 @app.get("/students/{student_id}")
-def get_student(student_id: int):
+async def get_student(student_id: int):
     """Возвращение информации о студенте"""
-    student = students[student_id]
-    return {
-        "name": student["name"],
-        "age": student["age"],
-        "grades": student["grades"],
-        "thesis_topic": student["thesis_topic"],
-    }
-
+    student: Union[Student, GraduateStudent] = students[student_id]
+    return student.model_dump()
